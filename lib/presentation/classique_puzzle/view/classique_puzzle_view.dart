4f4c80b4bcdd57495e3b6487cf60f89c962b5e9a
Sameh_Gbuibia <gbuibiasameh@issatso.u-sousse.tn>
2022-12-19 21:45:01 +0100

    second commit 2

--- lib/presentation/classique_puzzle/view/classique_puzzle_view.dart
+++ lib/presentation/classique_puzzle/view/classique_puzzle_view.dart
@@ -40,7 +40,6 @@ class _ClassiquePuzzleViewState extends State<ClassiquePuzzleView> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
     return Scaffold(
       backgroundColor: ColorsManager.lightOrange,
       floatingActionButtonLocation: context.locale == ARABIC_LOCAL
